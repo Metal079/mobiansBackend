beae21b06513a2812debad68c79a26f49a6f6536
metal079 <pablocastro079@gmail.com>
2024-04-01 23:46:07 -0500

    revert april fools

--- helper_functions.py
+++ helper_functions.py
@@ -9,7 +9,7 @@ from PIL import Image, ImageDraw, ImageFont, PngImagePlugin
 # ---- image manipulation -----
 
 # Create the watermark image and font objects outside the function
-#watermark_text = "Mobians.ai"
+watermark_text = "Mobians.ai"
 opacity = 128
 font_file_path = r"fonts/Roboto-Medium.ttf"
 font = ImageFont.truetype(font_file_path, 25)
@@ -65,10 +65,6 @@ async def add_image_metadata(image, request_data):
 
 
 async def add_watermark(image):
-    # randomly pick between EggmanEmpire.ai or Plumbers.ai
-    options = ["JSMetal.com", "EggmanEmpire.ai", "tweetrdot.com", "y.com", "/r/SonicTheHedgehog", "Plumbers.ai", "Moon says he loves you", "Mobian.pie", "01001100", "Sonic can see you sin", "Try Minecraft!", "Check out jscammie.com!", "original character donut steal", "Sonic says: 'Don't do drugs'", "Sonic says: 'Don't do school'", "Sonic says: 'Don't do milk'", "Sonic says: 'Don't do vegetables'", "Sonic says: 'Don't do homework'", "Moon thinks Joe smells", "AI girls dont love you", "Mobiance.ai", "AI was a mistake", ":ChaoDance:", "NecoSageIsWatching"]
-    watermark_text = random.choice(options)
-
     watermark = Image.new("RGBA", image.size, (255, 255, 255, 0))
     draw = ImageDraw.Draw(watermark)
 
